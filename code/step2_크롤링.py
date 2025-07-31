7a6ec386e3ce21b8453cd6696e9cdd8a1e69d527
SeungMin <dolkuji1@naver.com>
2025-07-31 15:35:54 +0900

    +

--- code/step2_크롤링.py
+++ code/step2_크롤링.py
@@ -38,7 +38,7 @@ class Step2:
         }
 
 
-        MAX_RESULTS = 10000 # 전체 긁어오는 특허수 조절변수
+        MAX_RESULTS = 1000 # 전체 긁어오는 특허수 조절변수
         MAX_PAGES=10
         url = 'http://plus.kipris.or.kr/kipo-api/kipi/patUtiModInfoSearchSevice/getWordSearch'
         
